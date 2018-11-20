8230a02c5d19fbf81a7e6f159afc0aae8b4c844b
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2018-11-20 02:56:09 +0000

    Added insecure mode feature to the server

--- server/Server.py
+++ server/Server.py
@@ -1,29 +1,42 @@
 import http.server, ssl
 from handlers.HTTPRequestHandler import HTTPRequestHandler
+from Logger import ConsoleLogger
 
 
 class Server:
-    def __init__(self, address, port):
+    def __init__(self, address, port, secure=True):
         self.address = address
         self.port = port
         self.certificate = "cert.pem"
         self.privateKey = "key.pem"
         self.httpServer = http.server.HTTPServer((self.address, self.port), HTTPRequestHandler)
+        self.logger = ConsoleLogger()
 
-        #SSL/TLS allowed versions. Forces the client to use TLS 1.2
-        #sslTLSVersion = ssl.PROTOCOL_TLSv1_2
-        #sslTLSVersion |= ssl.OP_NO_SSLv3
-        #sslTLSVersion |= ssl.OP_NO_TLSv1
-        #sslTLSVersion |= ssl.OP_NO_TLSv1_1
+        #Insecure socket (plain text on the network)
+        self.insecureSocket = self.httpServer.socket
 
-        #Wraps the server socket to use tls
-        #self.httpServer.socket = ssl.wrap_socket(self.httpServer.socket,
-               #server_side=True,
-               #keyfile=self.privateKey,
-               #certfile=self.certificate,
-               #ssl_version=sslTLSVersion
-        #)
+        #Secure socket, uses TLS 1.2
+        self.secureSocket = None
+
+
+        #If secure==True then wraps the server socket with a socket using TLS 1.2
+        if(secure == True):
+            #SSL/TLS allowed versions. Forces the client to use TLS 1.2
+            sslTLSVersion = ssl.PROTOCOL_TLSv1_2
+
+            #Wraps the server socket to use tls
+            self.secureSocket = ssl.wrap_socket(self.httpServer.socket,
+                   server_side=True,
+                   keyfile=self.privateKey,
+                   certfile=self.certificate,
+                   ssl_version=sslTLSVersion
+            )
+
+            self.httpServer.socket = self.secureSocket
+        else:
+            self.logger.warning("The server is running in insecure mode!")
+
 
     def start(self):
-        print("Server started at port: "+str(self.port))
+        self.logger.info("Server started at port: "+str(self.port))
         self.httpServer.serve_forever()
