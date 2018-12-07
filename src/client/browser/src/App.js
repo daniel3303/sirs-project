efa46c8b65585d102c3834ffb21251f45c97fdab
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2018-12-07 01:30:00 +0000

    Added Login Form

--- src/client/browser/src/App.js
+++ src/client/browser/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './components/LoginForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props){
+            super(props);
+
+    };
+
+    componentDidMount = () => {
+        //this.state
+    }
+
+    render() {
+        return (
+            <LoginForm />
+        );
+    };
 }
 
-export default App;
+export default App
